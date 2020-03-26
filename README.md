d2b2b8592077c4217e7bca448543f99c2280047c
Dario Donou <donoudario63@gmail.com>
2020-03-26 14:15:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# MeanChat
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.2.7.  It is the results of following the tutorial [here](https://www.djamware.com/post/58e0d15280aca75cdc948e4e/building-chat-application-using-mean-stack-angular-4-and-socketio).
+
+## Installation
+
+git clone this repository.  Then, from the root:
+
+```
+npm install
+ng build
+npm start
+```
+For the last step you can do this instead:
+```
+nodemon
+```
+However, when you change the angular, you have to do an `ng build`.
+
+To use the chat, open two browsers at:
+
+http://localhost:9000/
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
+
+import { ChatService } from './chat.service';
+
+import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+		HttpModule,
+  ],
+  providers: [
+    ChatService,
+  ],
+  bootstrap: [
+    AppComponent,
+  ]
+})
+export class AppModule { }
